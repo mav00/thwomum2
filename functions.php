ce858f2db344bbf753790484d6571c5e1743efe0
mav <mav@tuxbook.lan>
2021-01-31 14:47:12 +0100

    added Widget functions

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
 if ( function_exists('register_sidebar') )
-register_sidebar();
+    register_sidebar(array(
+        'name'          => esc_html__( 'Sidebar', 'thwomum2' ),
+        'id'            => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ));
 ?>
